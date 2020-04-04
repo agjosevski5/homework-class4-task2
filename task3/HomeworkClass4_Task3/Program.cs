5446de608d8d896e9680f90bcb56fcce5f170a26
Aleksej <aleksejgjosevski5@gmail.com>
2020-04-04 21:05:51 +0200

    HomeworkClass4_Task3

---
+++ task3/HomeworkClass4_Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeworkClass4_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myBirthday = Convert.ToDateTime("09/13/2000");
+            int age = AgeCalculator(myBirthday);
+            Console.WriteLine($"I'm {age} years old");
+            Console.ReadLine();
+        }
+
+        static int AgeCalculator(DateTime myBirthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - myBirthday.Year;
+
+            if (myBirthday > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
